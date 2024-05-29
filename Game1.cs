97409e6059ef9232ab68cb0fc1d618e8dcd8e3e6
jess-nl <jnoriegalessard@gmail.com>
2024-05-29 14:57:58 -0400

    refactor: create sprite class

--- Game1.cs
+++ Game1.cs
@@ -6,12 +6,12 @@ namespace CombatGame
 {
     public class Game1 : Game
     {
-        Texture2D ballTexture;
-        Vector2 ballPosition;
-        float ballSpeed;
-
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Sprite _sprite;
+
+        public const float SPRITE_SPEED = 100f;
 
         public Game1()
         {
@@ -23,11 +23,6 @@ namespace CombatGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            ballPosition = new Vector2(
-                _graphics.PreferredBackBufferWidth / 2,
-                _graphics.PreferredBackBufferHeight / 2
-            );
-            ballSpeed = 100f;
 
             base.Initialize();
         }
@@ -37,7 +32,8 @@ namespace CombatGame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            ballTexture = Content.Load<Texture2D>("ball");
+            var texture = Content.Load<Texture2D>("ball");
+            _sprite = new Sprite(texture, Vector2.Zero, SPRITE_SPEED);
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,26 +45,26 @@ namespace CombatGame
             var kState = Keyboard.GetState();
 
             if (kState.IsKeyDown(Keys.Up))
-                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                _sprite.position.Y -= _sprite.speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (kState.IsKeyDown(Keys.Down))
-                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                _sprite.position.Y += _sprite.speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (kState.IsKeyDown(Keys.Left))
-                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                _sprite.position.X -= _sprite.speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (kState.IsKeyDown(Keys.Right))
-                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                _sprite.position.X += _sprite.speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
-                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
-            else if (ballPosition.X < ballTexture.Width / 2)
-                ballPosition.X = ballTexture.Width / 2;
+            if (_sprite.position.X > _graphics.PreferredBackBufferWidth - _sprite.texture.Width / 2)
+                _sprite.position.X = _graphics.PreferredBackBufferWidth - _sprite.texture.Width / 2;
+            else if (_sprite.position.X < _sprite.texture.Width / 2)
+                _sprite.position.X = _sprite.texture.Width / 2;
 
-            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
-                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
-            else if (ballPosition.Y < ballTexture.Height / 2)
-                ballPosition.Y = ballTexture.Height / 2;
+            if (_sprite.position.Y > _graphics.PreferredBackBufferHeight - _sprite.texture.Height / 2)
+                _sprite.position.Y = _graphics.PreferredBackBufferHeight - _sprite.texture.Height / 2;
+            else if (_sprite.position.Y < _sprite.texture.Height / 2)
+                _sprite.position.Y = _sprite.texture.Height / 2;
 
             base.Update(gameTime);
         }
@@ -78,14 +74,14 @@ namespace CombatGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            _spriteBatch.Begin();
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
             _spriteBatch.Draw(
-                ballTexture,
-                ballPosition,
+                _sprite.texture,
+                _sprite.position,
                 null,
                 Color.White,
                 0f,
-                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                new Vector2(_sprite.texture.Width / 2, _sprite.texture.Height / 2),
                 Vector2.One,
                 SpriteEffects.None,
                 0f
---
+++ Sprite.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CombatGame
+{
+    public class Sprite
+    {
+        public Texture2D texture;
+        public Vector2 position;
+        public float speed;
+
+        public Sprite(Texture2D texture, Vector2 position, float speed)
+        {
+            this.texture = texture;
+            this.position = position;
+            this.speed = speed;
+        }
+    }
+}
