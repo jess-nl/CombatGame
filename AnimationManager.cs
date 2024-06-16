0afc45d0e2c3a25378b5e0158762e33cc14b2fae
jess-nl <jnoriegalessard@gmail.com>
2024-06-16 17:11:55 -0400

    feat: pointer for sprite frame start & end

--- AnimationManager.cs
+++ AnimationManager.cs
@@ -12,18 +12,31 @@ namespace CombatGame
         int activeFrame;
         int interval;
 
+        int startFrame;
+        int? endFrame;
+
         int rowPos;
         int colPos;
 
-        public AnimationManager(int numFrames, int numColumns, Vector2 size)
+        public AnimationManager(
+            int numFrames,
+            int numColumns,
+            Vector2 size,
+            int startFrame = 0,
+            int? endFrame = null
+            )
         {
             this.numFrames = numFrames;
             this.numColumns = numColumns;
             this.size = size;
+            this.startFrame = startFrame;
+            // Similarly: this.endFrame = endFrame.HasValue ? endFrame.Value : numFrames - 1;
+            this.endFrame = endFrame ?? numFrames - 1;
 
             counter = 0;
-            activeFrame = 0;
             interval = 18;
+
+            ResetAnimation();
         }
 
         public void Update()
@@ -40,23 +53,32 @@ namespace CombatGame
         {
             activeFrame++;
             colPos++;
-            if (activeFrame >= numFrames)
-            {
+            if (activeFrame >= endFrame)
                 ResetAnimation();
-            }
 
             if (colPos >= numColumns)
             {
                 colPos = 0;
                 rowPos++;
             }
+
+            var firstFrameInRow = rowPos * numColumns;
+            var lastFrameInRow = firstFrameInRow + colPos;
+            if (lastFrameInRow >= endFrame)
+                ResetAnimation();
+        }
+
+        public void ChangeFrames(int start, int end)
+        {
+            startFrame = start;
+            endFrame = end;
         }
 
         private void ResetAnimation()
         {
-            activeFrame = 0;
-            colPos = 0;
-            rowPos = 0;
+            activeFrame = startFrame;
+            colPos = startFrame % numColumns;
+            rowPos = startFrame / numColumns;
         }
 
         public Rectangle GetFrame()
