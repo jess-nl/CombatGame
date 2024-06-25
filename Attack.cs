b3299c84152154657f9b3c7eee529606f2f5a0b7
jess-nl <jnoriegalessard@gmail.com>
2024-06-25 09:47:33 -0400

    feat: create attack class

---
+++ Attack.cs
@@ -0,0 +1,16 @@
+﻿namespace CombatGame
+{
+    public class Attack
+    {
+        public int Kick { get; set; }
+        public int Spin { get; set; }
+        public int WandAttack { get; set; }
+
+        public Attack(int kick, int spin, int wandAttack)
+        {
+            Kick = kick;
+            Spin = spin;
+            WandAttack = wandAttack;
+        }
+    }
+}
